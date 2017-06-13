fec8863cb134f2ca176a3a4a70429a7369169ae7
aldolipani <aldo.lipani@gmail.com>
2017-06-13 15:48:44 -0400

    backend

--- backend/target/scala-2.11/twirl/main/views/html/index.template.scala
+++ backend/target/scala-2.11/twirl/main/views/html/index.template.scala
@@ -33,14 +33,14 @@ Seq[Any](format.raw/*1.4*/("""
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="visual-pool/config/environment" content="%7B%22modulePrefix%22%3A%22visual-pool%22%2C%22environment%22%3A%22production%22%2C%22rootURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22visual-pool%22%2C%22version%22%3A%220.0.0+7d98136b%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
-    <link rel="stylesheet" href="/assets/vendor.css" integrity="sha256-5TkWZwOs25hrG91gTZk3XPjJe8d4yhWtIFcK4p/Pm0s= sha512-HvxN2lh5wRYcHURug/yfoHGetnRqL4oUt4ZCZCuirm4hxOjJIr3ezBvSyPGYRNFGkpXxF6O0QrQhKZJHAC6aaA==" >
-    <link rel="stylesheet" href="/assets/visual-pool.css" integrity="sha256-g+mZ0MHrAtDcysfZhrp8g/qVjVQHgwLhjOjCKXHKnNg= sha512-MMm2zcluinWKQEbWa7ygS7zJfbJqUJLSVJZoYy1Mp/iu5oysk4jkk9G7XRpkUcBs0yOxCrD5K8DWfjD4HLPuHw==" >
+    <link rel="stylesheet" href="/assets/vendor.css">
+    <link rel="stylesheet" href="/assets/visual-pool.css">
   </head>
 
   <body>
 
-  <script src="/assets/vendor.js" integrity="sha256-kO2uI3zf1guPrnI0+xY0GDMaCeagIL7ACucPjOlvmFg= sha512-OjVndTFvEUcTGUIQC8JNmzCe38s7x29quqkQXh3BGQGBf26OPmxkeh1wXblsVbG7R8G/kv9L0X9GC1xCa0jjWQ==" ></script>
-  <script src="/assets/visual-pool.js" integrity="sha256-aiy/8x8bltZFdsn+RAiz6A7qWz2fhfrSTb/E2raReyg= sha512-Ju/92YWPGzJoo8Aixzpj5vU4cJZv1y79Dvokubyyy7OCAJon8f2ISX2epZ17NbFQXJhzkQJu6fVqsXLE5NIDew==" ></script>
+  <script src="/assets/vendor.js"></script>
+  <script src="/assets/visual-pool.js" ></script>
 
   <div id="ember-bootstrap-wormhole"></div>
   </body>
@@ -66,9 +66,9 @@ Seq[Any](format.raw/*1.4*/("""
 object index extends index_Scope0.index
               /*
                   -- GENERATED --
-                  DATE: Wed May 10 16:45:28 EDT 2017
+                  DATE: Tue Jun 13 15:45:56 EDT 2017
                   SOURCE: /Users/aldo/Projects/GitHub/VisualPool/backend/app/views/index.scala.html
-                  HASH: dbf363237452ee7d5f3c01315166013a3a805b08
+                  HASH: 7397bfa8ecdc38ed56a11441f28359eb050620b8
                   MATRIX: 520->1|616->3|643->4
                   LINES: 20->1|25->1|26->2
                   -- GENERATED --
